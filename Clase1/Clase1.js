3b71f255b2bdedde30c012a8bf4c298c1e0d9b90
Rosario Begani <rochibega@gmail.com>
2021-10-30 15:51:56 -0300

    Ejercicios 1 y 2

---
+++ Clase1/Clase1.js
@@ -0,0 +1,7 @@
+let Nombre = prompt("Ingrese su Nombre");
+console.log (Nombre);
+
+let a = (21);
+let b = prompt("Ingrese un numero")
+let suma = a + b ;
+console.log ("El resultado es " + suma );
---
+++ clase2/Clase2.js
@@ -0,0 +1,15 @@
+let Nombre = prompt("Ingrese su Nombre");
+console.log (Nombre);
+
+let a = (21);
+let b = prompt("Ingrese un numero")
+let suma = a + b ;
+console.log ("El resultado es " + suma );
+
+let Numero = prompt("Ingresa un numero");
+if( Numero >= 216 ){
+    alert ("Holi :-)");
+}
+else{
+    console.log("Chau :-(");
+}
